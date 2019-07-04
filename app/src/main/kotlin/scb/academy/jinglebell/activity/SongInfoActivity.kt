9449b525acc45606b0d980bd2b87dbde2dedcbb7
last3oy <softwarez_gelion@hotmail.com>
2019-07-04 09:36:57 +0700

    continue song info

--- app/src/main/kotlin/scb/academy/jinglebell/activity/SongInfoActivity.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/activity/SongInfoActivity.kt
@@ -1,15 +1,17 @@
 package scb.academy.jinglebell.activity
 
-import android.app.Activity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import scb.academy.jinglebell.R
 import scb.academy.jinglebell.vo.Song
+import java.text.SimpleDateFormat
 
-class SongInfoActivity : Activity() {
+class SongInfoActivity : AppCompatActivity() {
 
-    private lateinit var ivSognArtWork: ImageView
+    private lateinit var ivSongArtWork: ImageView
     private lateinit var tvSongName: TextView
     private lateinit var tvSongAlbum: TextView
     private lateinit var tvSongReleaseDate: TextView
@@ -21,7 +23,7 @@ class SongInfoActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_song_info)
 
-        ivSognArtWork = findViewById(R.id.iv_song_artwork)
+        ivSongArtWork = findViewById(R.id.iv_song_artwork)
         tvSongName = findViewById(R.id.tv_song_name)
         tvSongAlbum = findViewById(R.id.tv_song_album)
         tvSongReleaseDate = findViewById(R.id.tv_release_date)
@@ -31,6 +33,18 @@ class SongInfoActivity : Activity() {
 
         val song = intent.getParcelableExtra<Song>("song")
 
+        setTitle("${song.artistName}")
+
+        Picasso.get()
+                .load(song.artworkUrl)
+                .into(ivSongArtWork)
         tvSongName.text = song.name
+        tvSongAlbum.text = song.album
+        val date = SimpleDateFormat("YYYY-MM-dd'T'HH:mm:ss'Z'").parse(song.releaseDate)
+        val out = SimpleDateFormat("YYYY-MM-dd")
+        tvSongReleaseDate.text = out.format(date).toString()
+        tvSongGenre.text = song.genre
+        tvSongTrackPrice.text = "${song.price} ${song.priceCurrency}"
+        tvSongCollectionPrice.text = "${song.collectionPrice} ${song.priceCurrency}"
     }
 }
